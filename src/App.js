bbf46238ecd07a78f0347d7047ddbde3c5575dcb
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-01-18 20:08:34 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header';
-
+import trollFace from './img/'
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         
         <Header>
-          <img src="./img/trollFace.png" alt="trollFace"></img>
+          <img src="./src/img/trollFace.png" alt="trollFace"></img>
           <h2>Hi</h2>
         </Header>
       </header>
