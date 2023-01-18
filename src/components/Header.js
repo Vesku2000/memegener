8b2900fd38c4c714d621964e3598d08fa6992940
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-01-18 20:08:46 +0200

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@ import React from "react"
 
 export default function Header() {
     return (
+        <div>
+            <img src="./img/trollFace.png" alt="trollFace" />
         <h1>Header</h1>
+        </div>
     )
 }
